27c09eedcef229b834fb8b723c8814c71b422509
edzhang10 <45860266+edzhang10@users.noreply.github.com>
2019-01-22 18:44:34 -0500

    Add files via upload

---
+++ Turtle Spiral 4.py
@@ -0,0 +1,9 @@
+import turtle
+t = turtle.Pen()
+colors = ["red", "purple", "blue", "orange"]
+for x in range(100):
+    t.pencolor( colors[ x % 4]
+                t.forward(2*x)
+                t.left(90)
+    
+    
